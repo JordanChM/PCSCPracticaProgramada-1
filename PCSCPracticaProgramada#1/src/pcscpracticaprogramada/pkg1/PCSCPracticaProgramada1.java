3fb5cbbbfcd86ff650c2b06ac75dae7b2539426c
Franciscoffv <franffv0809@gmail.com>
2024-10-13 18:25:28 -0600

    PCSCPracticaProgramada1.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/PCSCPracticaProgramada1.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/PCSCPracticaProgramada1.java
@@ -14,21 +14,33 @@ public class PCSCPracticaProgramada1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Autor autor = new Autor("John", "Tolkien");
-        Libro libro = new Libro("El senior de los anillos", autor, 1954, true);
-        Persona usuario = new Usuario("12-10-24","Alex", "alex@gmail.com");
+         Autor autor = new Autor("John", "Tolkien");
+        Libro libro1 = new Libro("El senior de los anillos", autor, 1954, true);
+       
+        autor.agregarLibro(libro1);
+
+
+        Usuario usuario = new Usuario("12-10-2024", "Juan Pérez", "alex@gmail.com");
         
-        autor.agregarLibro(libro);
-        
-        Prestamo prestamor = new PrestamoRegular();
-        Prestamo prestamou = new PrestamoUrgente();
-        
-        try {
-            p
+
+        System.out.println("Autor: " + autor.getNombre() + " " + autor.getApellido());
+        System.out.println("Libros escritos: ");
+        for (Libro libro : autor.getLibros()) {
+            System.out.println(" " + libro.getTitulo() + " " + libro.getAnioPublicacion() + " ");
         }
-        prestamor.calcularCosto();
-        prestamou.calcularCosto();
-        
-        prestamor.
+
+        try {
+
+            PrestamoRegular prestamo = new PrestamoRegular(libro1, usuario);
+            prestamo.validarPrestamo(libro1, usuario); 
+            prestamo.calcularCosto(); 
+
+            PrestamoUrgente prestamoUrgente = new PrestamoUrgente(libro1, usuario);
+            prestamoUrgente.calcularCosto(); 
+
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
     }
 }
