8a636852507ae1d425d6c1373c4b9fa152dedda4
AndyHR14 <andypipoh@gmail.com>
2024-10-13 18:29:45 -0600

    Update PrestamoRegular.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/PrestamoRegular.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/PrestamoRegular.java
@@ -12,20 +12,19 @@ import javax.swing.JOptionPane;
  */
 public class PrestamoRegular extends Prestamo{
     private int duracion;
-    private int costo = 360;
+    private int costoPorDia = 360;
 
     public PrestamoRegular(Libro libro, Usuario usuario) {
         super(libro, usuario);
     }
 
     @Override
-    public void calcularCosto(){
+    public int calcularCosto(){
         int costoFinal;
         duracion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de dias que desea poseer el libro: "));
-        costoFinal = duracion*costo;
-        JOptionPane.showMessageDialog(null, "El costo del prestamo sera de: " + costoFinal);
-        libro.setDisponible(false);
-        }
+        costoFinal = duracion*costoPorDia;
+        return costoFinal;
+    }
     
     public void validarPrestamo(Libro libro, Usuario usuario) throws Exception {
         if (!libro.isDisponible()) {
@@ -35,4 +34,15 @@ public class PrestamoRegular extends Prestamo{
             throw new Exception("El usuario no está registrado");
         }
     }
+    public void realizarPrestamo(Libro libro, Usuario usuario){
+        try{
+            validarPrestamo(libro, usuario);
+            int costo = calcularCosto();
+            JOptionPane.showMessageDialog(null, "El usuario: " +usuario+ " realizo un prestamo de: " +costo+ " por el libro: " +libro+ " exitosamente!");
+            libro.setDisponible(false);
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        
+    }   
 }
