b7169614b02ef733ba6edc70f82f95adada72415
luckschavalo <lucaschl1007@gmail.com>
2024-10-13 21:43:28 -0600

    Add files via upload

---
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Gestion.java
@@ -0,0 +1,19 @@
+
+package pcscpracticaprogramada.pkg1;
+
+
+public class Gestion extends Libro{
+    private Libro[] libros;
+ 
+    public Gestion(Libro[] libros, String titulo, Autor autor, int anioPublicacion, boolean disponible) {
+        super(titulo, autor, anioPublicacion, disponible);
+        this.libros = libros;
+    }
+    
+    
+public void mostrarLibros(){
+ for (int i = 0; i > libros.length; i++){
+     
+       }   
+    }
+}
